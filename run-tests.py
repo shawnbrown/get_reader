981b95d7b9f54462f9dc525339b5f58ef081f546
Shawn Brown <shawnbrown@users.noreply.github.com>
2019-08-15 21:59:51 -0400

    Change run-tests.py to return a proper exit code.

--- run-tests.py
+++ run-tests.py
@@ -21,4 +21,8 @@ if __name__ == '__main__':
     # Handle test-discovery explicitly for Python 2.6 compatibility.
     start_dir = os.path.abspath(os.path.dirname(__file__))
     testsuite = unittest.TestLoader().discover(start_dir)
-    unittest.TextTestRunner().run(testsuite)
+    result = unittest.TextTestRunner().run(testsuite)
+    if result.wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)

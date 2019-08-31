bbab5cf25fc63bb999b883ea9635dd2686ab00a3
Shawn Brown <shawnbrown@users.noreply.github.com>
2019-08-31 15:24:12 -0400

    Move pandas test cases into into their own file.
    This is being done to help organize the test suite into more easily
    discoverable sections. The single test file was over 1000 lines and
    threatening to become cumbersome for new contributors.

---
+++ tests/test_from_pandas.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+try:
+    import pandas
+except ImportError:
+    pandas = None
+
+from get_reader import _from_pandas
+
+
+@unittest.skipIf(not pandas, 'pandas not found')
+class TestFromPandas(unittest.TestCase):
+    def setUp(self):
+        self.df = pandas.DataFrame({
+            'col1': (1, 2, 3),
+            'col2': ('a', 'b', 'c'),
+        })
+
+    def test_automatic_indexing(self):
+        reader = _from_pandas(self.df)  # <- Includes index by default.
+        expected = [
+            [None, 'col1', 'col2'],
+            [0, 1, 'a'],
+            [1, 2, 'b'],
+            [2, 3, 'c'],
+        ]
+        self.assertEqual(list(reader), expected)
+
+        reader = _from_pandas(self.df, index=False)  # <- Omits index.
+        expected = [
+            ['col1', 'col2'],
+            [1, 'a'],
+            [2, 'b'],
+            [3, 'c'],
+        ]
+        self.assertEqual(list(reader), expected)
+
+    def test_simple_index(self):
+        self.df.index = pandas.Index(['x', 'y', 'z'], name='col0')
+
+        reader = _from_pandas(self.df)
+        expected = [
+            ['col0', 'col1', 'col2'],
+            ['x', 1, 'a'],
+            ['y', 2, 'b'],
+            ['z', 3, 'c'],
+        ]
+        self.assertEqual(list(reader), expected)
+
+        reader = _from_pandas(self.df, index=False)
+        expected = [
+            ['col1', 'col2'],
+            [1, 'a'],
+            [2, 'b'],
+            [3, 'c'],
+        ]
+        self.assertEqual(list(reader), expected)
+
+    def test_multiindex(self):
+        index_values = [('x', 'one'), ('x', 'two'), ('y', 'three')]
+        index = pandas.MultiIndex.from_tuples(index_values, names=['A', 'B'])
+        self.df.index = index
+
+        reader = _from_pandas(self.df)
+        expected = [
+            ['A', 'B', 'col1', 'col2'],
+            ['x', 'one', 1, 'a'],
+            ['x', 'two', 2, 'b'],
+            ['y', 'three', 3, 'c'],
+        ]
+        self.assertEqual(list(reader), expected)
+
+        reader = _from_pandas(self.df, index=False)
+        expected = [
+            ['col1', 'col2'],
+            [1, 'a'],
+            [2, 'b'],
+            [3, 'c'],
+        ]
+        self.assertEqual(list(reader), expected)
--- tests/test_get_reader.py
+++ tests/test_get_reader.py
@@ -43,7 +43,6 @@ from get_reader import _from_csv_iterable
 from get_reader import _from_csv_path
 from get_reader import _from_dicts
 from get_reader import _from_namedtuples
-from get_reader import _from_pandas
 from get_reader import _from_datatest
 from get_reader import _from_excel
 from get_reader import _from_dbf
@@ -619,78 +618,6 @@ class TestFromDatatest(unittest.TestCase):
         self.assertEqual(reader_list[0], ('foo', 'bar'))
         self.assertEqual(reader_list.count(('x', 1)), 2)
         self.assertEqual(reader_list.count(('y', 2)), 1)
-
-
-@unittest.skipIf(not pandas, 'pandas not found')
-class TestFromPandas(unittest.TestCase):
-    def setUp(self):
-        self.df = pandas.DataFrame({
-            'col1': (1, 2, 3),
-            'col2': ('a', 'b', 'c'),
-        })
-
-    def test_automatic_indexing(self):
-        reader = _from_pandas(self.df)  # <- Includes index by default.
-        expected = [
-            [None, 'col1', 'col2'],
-            [0, 1, 'a'],
-            [1, 2, 'b'],
-            [2, 3, 'c'],
-        ]
-        self.assertEqual(list(reader), expected)
-
-        reader = _from_pandas(self.df, index=False)  # <- Omits index.
-        expected = [
-            ['col1', 'col2'],
-            [1, 'a'],
-            [2, 'b'],
-            [3, 'c'],
-        ]
-        self.assertEqual(list(reader), expected)
-
-    def test_simple_index(self):
-        self.df.index = pandas.Index(['x', 'y', 'z'], name='col0')
-
-        reader = _from_pandas(self.df)
-        expected = [
-            ['col0', 'col1', 'col2'],
-            ['x', 1, 'a'],
-            ['y', 2, 'b'],
-            ['z', 3, 'c'],
-        ]
-        self.assertEqual(list(reader), expected)
-
-        reader = _from_pandas(self.df, index=False)
-        expected = [
-            ['col1', 'col2'],
-            [1, 'a'],
-            [2, 'b'],
-            [3, 'c'],
-        ]
-        self.assertEqual(list(reader), expected)
-
-    def test_multiindex(self):
-        index_values = [('x', 'one'), ('x', 'two'), ('y', 'three')]
-        index = pandas.MultiIndex.from_tuples(index_values, names=['A', 'B'])
-        self.df.index = index
-
-        reader = _from_pandas(self.df)
-        expected = [
-            ['A', 'B', 'col1', 'col2'],
-            ['x', 'one', 1, 'a'],
-            ['x', 'two', 2, 'b'],
-            ['y', 'three', 3, 'c'],
-        ]
-        self.assertEqual(list(reader), expected)
-
-        reader = _from_pandas(self.df, index=False)
-        expected = [
-            ['col1', 'col2'],
-            [1, 'a'],
-            [2, 'b'],
-            [3, 'c'],
-        ]
-        self.assertEqual(list(reader), expected)
 
 
 @unittest.skipIf(not sqlite3, 'sqlite3 not found')
